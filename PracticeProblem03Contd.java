cbf58513cacfae219ed26682e4d0bb5baef4f59b
AL-AbidRahman <alabidrahman1019@gmail.com>
2025-05-20 09:17:42 +0600

    Add files via upload

---
+++ PracticeProblem03Contd.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class PracticeProblem03Contd {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = scanner.nextInt();
+        int[] numbers = new int[n];
+
+        System.out.println("Enter the numbers:");
+        for (int i = 0; i < n; i++) {
+            numbers[i] = scanner.nextInt();
+        }
+
+        int max = numbers[0];
+        for (int i = 1; i < n; i++) {
+            if (numbers[i] > max) {
+                max = numbers[i];
+            }
+        }
+        System.out.println("Greatest number in the series: " + max);
+
+        System.out.print("\nEnter a number to check if it's prime: ");
+        int num = scanner.nextInt();
+        boolean isPrime = true;
+
+        if (num <= 1) {
+            isPrime = false;
+        } else {
+            for (int i = 2; i <= num / 2; i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        if (isPrime) {
+            System.out.println(num + " is a prime number.");
+        } else {
+            System.out.println(num + " is not a prime number.");
+        }
+
+        System.out.print("\nReversed series: ");
+        for (int i = n - 1; i >= 0; i--) {
+            System.out.print(numbers[i] + " ");
+        }
+
+        scanner.close();
+    }
+}
