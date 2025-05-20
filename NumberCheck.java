4a7ee8d1b738b39f18235eb889745205ce699d19
AL-AbidRahman <alabidrahman1019@gmail.com>
2025-05-20 09:03:15 +0600

    Add files via upload

---
+++ NumberCheck.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class NumberCheck {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter an integer: ");
+        int number = scanner.nextInt();
+
+        if (number > 0) {
+            System.out.println("The number is a positive integer.");
+        } else {
+            System.out.println("The number is not a positive integer.");
+        }
+
+        if (number % 2 == 0) {
+            System.out.println("The number is even.");
+        } else {
+            System.out.println("The number is odd.");
+        }
+
+        scanner.close();
+    }
+}
